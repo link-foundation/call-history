27be789c5a54b9395f466f02051fbaa7a050798f
FreePhoenix888 <freephoenix888@gmail.com>
2023-12-22 22:40:06 +0600

    export {CallHistory} from './call-history.js'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 export {WithCallHistorySync,WithCallHistorySyncOptions} from './react/components/with-call-history-sync.js'
 export {UseCallHistorySyncOptions,useCallHistorySync} from './react/hooks/use-call-history-sync.js'
+export {CallHistory} from './call-history.js'
 export {checkPermissions} from './checkPermissions.js'
 export {requestPermissions} from './requestPermissions.js'
 export {CALL_HISTORY_PACKAGE_NAME, CALL_HISTORY_PACKAGE_NAME as NAME, CALL_HISTORY_PACKAGE_NAME as PACKAGE_NAME} from './name.js'
